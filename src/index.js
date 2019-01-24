ad70030886d7735d4b6cde85695708fd9ee655d1
Amir <shamsi.amir@gmail.com>
2019-01-24 08:01:28 +0100

    Enable tranpiling JS ES6 to ES5

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const text = 'Hello John Doe'
+console.log(text);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
